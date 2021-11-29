72d21307f7187ae5541e366d64b8b0dceaf0b5ea
golsa444 <golsadorsamccoy@yahoo.com>
2021-11-29 13:56:02 +0100

    Change Enum and add GetTollFee. WIP...

--- src/Class/TollCalculator.java
+++ src/Class/TollCalculator.java
@@ -1,4 +1,40 @@
 package Class;
 
+import Enum.TimePeriod;
+import Interface.Vehicle;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
 public class TollCalculator {
+
+
+    public static void GetTollFee(Vehicle vehicle, List<LocalDateTime> dates) {
+
+        Vehicle c = new Car();
+        Vehicle mc = new MC();
+
+
+        List<LocalTime> times = dates.stream().map(LocalDateTime::toLocalTime).sorted().toList();
+        System.out.println(times);
+        // LocalTime intervalEnd = times.get(0).plusHours(1);
+        int totalFee = 0;
+        int maxFee = 0;
+        for (LocalTime time : times) {
+            if (TimePeriod.RushHourAM.TimeRange(time) || TimePeriod.RushHourPM.TimeRange(time)) {
+                totalFee = 18;
+            } else {
+                totalFee = 11;
+            }
+            maxFee += totalFee;
+            System.out.println(maxFee);
+        }
+    }
+
+
 }
+
+
+
